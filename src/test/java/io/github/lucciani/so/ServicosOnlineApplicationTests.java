929980db323609e5c4ccad8e04d77ca05c918246
giancarlolucciani <giancarlo.lucciani@opovodigital.com>
2022-04-27 20:49:53 -0300

    iniciando projeto

---
+++ src/test/java/io/github/lucciani/so/ServicosOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucciani.so;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicosOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
