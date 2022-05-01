3f099cfd569c6bf9a71a95143444c312c2e50cc8
lucciani <giancarlolucciani@gmail.com>
2022-05-01 10:37:59 -0300

    criando arquivo de configuração ModelMapperConfig

---
+++ src/main/java/io/github/lucciani/so/core/modelmapper/ModelMapperConfig.java
@@ -0,0 +1,18 @@
+package io.github.lucciani.so.core.modelmapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+	@Bean
+	public ModelMapper modelMapper() {
+
+		var modelMapper = new ModelMapper();
+
+		return modelMapper;
+	}
+
+}
