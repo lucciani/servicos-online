ba5aa5ad916cbf4793e3fb92f282303f336b8b5c
lucciani <giancarlolucciani@gmail.com>
2022-05-01 11:02:16 -0300

    criando repositorio customizavel

--- src/main/java/io/github/lucciani/so/ServicosOnlineApplication.java
+++ src/main/java/io/github/lucciani/so/ServicosOnlineApplication.java
@@ -1,12 +1,19 @@
 package io.github.lucciani.so;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import io.github.lucciani.so.infrastructure.CustomJpaRepositoryImpl;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class ServicosOnlineApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(ServicosOnlineApplication.class, args);
 	}
 
